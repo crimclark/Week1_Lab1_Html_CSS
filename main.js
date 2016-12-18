0a3c9b082ab906592f4c20c3dbd332d2cafd4ee0
Tim Clark <oeomusic@gmail.com>
2016-12-17 21:42:15 -0800

    adds link description

---
+++ main.js
@@ -0,0 +1,14 @@
+var description = document.querySelector('.description');
+
+var img = document.querySelector('img');
+
+function makeOpaque() {
+  description.classList.add('opaque');
+}
+
+function makeInvsbl() {
+  description.classList.remove('opaque');
+}
+
+img.addEventListener('mouseover', makeOpaque);
+img.addEventListener('mouseleave', makeInvsbl);
