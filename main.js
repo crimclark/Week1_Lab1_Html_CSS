00d5937fe813d57f49072f18b70817b8f4a88f65
Tim Clark <oeomusic@gmail.com>
2017-03-07 16:17:06 -0800

    finishes main portfolio

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
-var description = document.querySelector('.description');
+// var description = document.querySelector('.description');
 
-var img = document.querySelector('img');
+// var img = document.querySelector('img');
 
-function makeOpaque() {
-  description.classList.add('opaque');
-}
+// function makeOpaque() {
+//   description.classList.add('opaque');
+// }
 
-function makeInvsbl() {
-  description.classList.remove('opaque');
-}
+// function makeInvsbl() {
+//   description.classList.remove('opaque');
+// }
 
-img.addEventListener('mouseover', makeOpaque);
-img.addEventListener('mouseleave', makeInvsbl);
+// img.addEventListener('mouseover', makeOpaque);
+// img.addEventListener('mouseleave', makeInvsbl);
